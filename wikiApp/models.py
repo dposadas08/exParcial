4c5be115a1125d558c733a86549235b9d5f2ab66
dposadas08 <daposadas26@gmail.com>
2024-12-06 03:15:50 -0500

    Creación y migración de los modelos temaWiki y articuloWiki para la aplicación wikiApp

--- wikiApp/models.py
+++ wikiApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class temaWiki(models.Model):
+    nombre = models.CharField(max_length=128, null=True, blank=True)
+    descripcion = models.CharField(max_length=512, null=True, blank=True)
+    
+class articuloWiki(models.Model):
+    titulo = models.CharField(max_length=128, null=True, blank=True)
+    contenido = models.CharField(max_length=1024, null=True, blank=True)
+    temaR = models.ForeignKey(temaWiki, null=True, blank=True, on_delete=models.SET_NULL)
